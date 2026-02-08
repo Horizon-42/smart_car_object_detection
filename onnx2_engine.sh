835c18a53af181fa6cfafbf1da926ca417554aed
ldx <ldx@xiaofutech.com>
2026-02-08 15:20:12 +0100

    Refactor code structure for improved readability and maintainability

--- onnx2_engine.sh
+++
@@ -1,6 +0,0 @@
-/usr/src/tensorrt/bin/trtexec --onnx=yolo11_robust.onnx \
-          --saveEngine=yolo11_robust.engine \
-          --fp16 \
-          --workspace=1024 \
-          --buildOnly \
-          --verbose
